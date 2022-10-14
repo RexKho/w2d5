0e28704bdfd5cceaf3237d37555e04a5fbead4cc
Rex Kho <rexkho510@gmail.com>
2022-10-14 14:40:29 -0700

    <finished flight>

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -17,8 +17,31 @@ class Flight
 
     end
 
-    
+    def board_passenger(pass)
+        if full?
+            return
+        else
+            if pass.has_flight?(@flight_number)
+                @passengers << pass
+            end
 
+        end
+    end
 
+    def list_passengers
+        newarr = []
+        @passengers.each do |ele|
+            newarr << ele.name
+        end
+        newarr
+    end
 
+    def [](index)
+        @passengers[index]
+
+    end
+
+    def <<(pass)
+        board_passenger(pass)
+    end
 end
